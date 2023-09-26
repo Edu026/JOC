76901871b1ffb0ae4d2ec29c141de2588c81bdba
«@Edu026» <edu.amoros26@gmail.com>
2023-09-26 17:09:51 +0200

    TextView show random number

--- app/src/main/java/com/example/joc/MainActivity.java
+++ app/src/main/java/com/example/joc/MainActivity.java
@@ -23,8 +23,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         intRandom = rand.nextInt(10);
+
         final Button button = findViewById(R.id.button_1);
         final EditText answer  = findViewById(R.id.editTextNumber);
+        //TextView viewRandom = (TextView)findViewById(R.id.textView2);
+        //viewRandom.setText(intRandom);
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -33,11 +36,8 @@ public class MainActivity extends AppCompatActivity {
                 int duration = Toast.LENGTH_SHORT;
                 Toast toast = Toast.makeText(MainActivity.this, text, duration);
                 toast.show();
+
                 //Log.i("INFO","El número introduït és: " + intRandom);
-                Log.i("INFO","El número introduït és: " + answer);
-
-
-
 
             }
         });
