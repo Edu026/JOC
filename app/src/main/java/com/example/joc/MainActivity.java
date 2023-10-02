ac940c42176446c163b0f47315876cdd0ba53a9b
«@Edu026» <edu.amoros26@gmail.com>
2023-10-02 16:23:18 +0200

    TextView show random number

--- app/src/main/java/com/example/joc/MainActivity.java
+++ app/src/main/java/com/example/joc/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.joc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -25,19 +26,33 @@ public class MainActivity extends AppCompatActivity {
         intRandom = rand.nextInt(10);
 
         final Button button = findViewById(R.id.button_1);
-        final EditText answer  = findViewById(R.id.editTextNumber);
+        final EditText answer = findViewById(R.id.editTextNumber);
         //TextView viewRandom = (TextView)findViewById(R.id.textView2);
         //viewRandom.setText(intRandom);
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                int numberAnswer = Integer.parseInt(answer.getText().toString());
+                CharSequence text = "";
+                Log.i("INFO","El número random és: " + intRandom);
+                if (numberAnswer < intRandom) {
+                    text = "El número que buscas es mayor";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(MainActivity.this, text, duration);
+                    toast.show();
+                } else if (numberAnswer > intRandom) {
+                    text = "El número que buscas es menor";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(MainActivity.this, text, duration);
+                    toast.show();
+                } else if (numberAnswer == intRandom){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage(R.string.dialog_message);
+                    //.setTitle(R.string.dialog_title);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
 
-                CharSequence text = "Hello toast!";
-                int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText(MainActivity.this, text, duration);
-                toast.show();
-
-                //Log.i("INFO","El número introduït és: " + intRandom);
 
             }
         });
